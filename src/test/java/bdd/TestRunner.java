191bc260bfa7272a1ea0e6af09e3df59a8e7e8f6
HENRY <h.gonzal96@gmail.com>
2026-08-02 21:20:48 -0500

    nuevos cambios 9

--- src/test/java/bdd/TestRunner.java
+++ src/test/java/bdd/TestRunner.java
@@ -1,15 +1,16 @@
 package bdd;
 
-import com.intuit.karate.junit5.Karate;
+        import com.intuit.karate.junit5.Karate;
 
 public class TestRunner {
+
     @Karate.Test
     Karate testLogin() {
-        return Karate.run("loginAuth").relativeTo(getClass());
+        return Karate.run("classpath:bdd/auth/loginAuth.feature");
     }
 
     @Karate.Test
     Karate testRegister() {
-        return Karate.run("registerAuth").relativeTo(getClass());
+        return Karate.run("classpath:bdd/auth/registerAuth.feature");
     }
 }
